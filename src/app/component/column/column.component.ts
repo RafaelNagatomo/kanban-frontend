0d0551fc7e199d2cab21d730d1de3421d4336d2d
Rafael Nagatomo <rafaelnagatomo@gmail.com>
2024-11-28 12:27:58 -0300

    feat: delete column implemented

--- src/app/component/column/column.component.ts
+++ src/app/component/column/column.component.ts
@@ -7,23 +7,27 @@ import {
   Output,
   SimpleChanges
 } from '@angular/core'
-import { BehaviorSubject, Observable } from 'rxjs'
+import { Observable } from 'rxjs'
 import { IColumn } from '../../shared/interfaces/column.interface'
 import { GraphqlService } from '../../shared/graphql/graphql.service'
 import { GET_ALL_COLUMNS } from '../../shared/queries/column.queries'
 import { CommonModule } from '@angular/common'
 import { ColumnService } from '../../shared/services/column.services'
+import { ConfirmModalComponent } from '../../modals/confirm-modal/confirm-modal.component'
 
 @Component({
   selector: 'app-column',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ConfirmModalComponent],
   templateUrl: './column.component.html',
   styleUrl: './column.component.sass',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ColumnComponent implements OnChanges {
   columns$!: Observable<IColumn[]>
+  isDeleteColumnModalOpen: boolean = false
+  columnToDelete: IColumn = {}
+  errorMessage: string = ''
   
   @Input() boardId!: number
   @Output() emitOpenAddEditColumnModal = new EventEmitter<IColumn>()
@@ -50,7 +54,29 @@ export class ColumnComponent implements OnChanges {
     })
   }
 
+  openDeleteModal(column: IColumn): void {
+    this.columnToDelete = column
+    this.isDeleteColumnModalOpen = true
+  }
+
   openAddEditColumnModal(columnData?: IColumn) {
     this.emitOpenAddEditColumnModal.emit(columnData)
+  }
+
+  onDeleteColumnModalClosed(): void {
+    this.isDeleteColumnModalOpen = false
+  }
+
+  async handleDeletionColumn(): Promise<void> {
+    if (!this.columnToDelete) return
+    try {
+      const result = await this.columnService.deleteColumn(this.columnToDelete)
+      if (result) {
+        this.onDeleteColumnModalClosed()
+      }
+    } catch (error) {
+      console.error('Não foi possível concluir a operação:', error)
+      this.errorMessage = String(error)
+    }
   }
 }
--- src/app/shared/services/column.services.ts
+++ src/app/shared/services/column.services.ts
@@ -4,7 +4,8 @@ import { IColumn } from '../interfaces/column.interface'
 import { GraphqlService } from '../graphql/graphql.service'
 import {
   CREATE_COLUMN_MUTATION,
-  UPDATE_COLUMN_MUTATION
+  UPDATE_COLUMN_MUTATION,
+  DELETE_COLUMN_MUTATION
 } from '../commands/column.commands'
 
 @Injectable({ providedIn: 'root' })
@@ -54,17 +55,44 @@ export class ColumnService {
             if (updatedColumn) {
               const currentColumns = this.columnsSubject.value
               const updatedColumns = currentColumns.map(column =>
-              column.id === updatedColumn.id ? updatedColumn : column)
+                column.id === updatedColumn.id ? updatedColumn : column)
               this.columnsSubject.next(updatedColumns)
               resolve(updatedColumn)
             } else {
               console.error('Falha ao atualizar coluna')
               return resolve(null)
-            }          
+            }
           },
           error: (error) => {
             console.error('Erro ao atualizar coluna:', error)
-            reject(error);
+            reject(error)
+          },
+        })
+    })
+  }
+
+  deleteColumn(deleteColumnData: Partial<IColumn>): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      this.graphqlService
+        .mutate(DELETE_COLUMN_MUTATION, { id: deleteColumnData.id })
+        .subscribe({
+          next: ({data}) => {
+            const deletedColum = data?.deleteColumn
+            
+            if (deletedColum) {
+              const currentColumns = this.columnsSubject.value
+              const updatedColumns = currentColumns.filter(column =>
+                column.id !== deleteColumnData.id)
+              this.columnsSubject.next(updatedColumns)
+              resolve(true)
+            } else {
+              console.error('Falha ao deletar a coluna')
+              return resolve(false)
+            }
+          },
+          error: (error) => {
+            console.error('Erro ao atualizar coluna:', error)
+            reject(error)
           },
         })
     })
